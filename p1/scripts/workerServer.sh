a6b3c302e473c970c66f7851a8ddcd910471700f
nisrinboukhari <nisrinboukhari19@gmail.com>
2025-11-04 10:34:23 +0100

    fix(p1): wait for the k3s_token from server

--- p1/scripts/workerServer.sh
+++ p1/scripts/workerServer.sh
@@ -1,3 +1,11 @@
+echo "Installing K3s agent..."
+
+# Wait for the token to be available from the server
+while [ ! -f /vagrant/k3s_token ]; do
+  echo "Waiting for k3s_token from server..."
+  sleep 5
+done
+
 TOKEN=$(cat /vagrant/k3s_token)
 
 curl -sfL https://get.k3s.io | \
@@ -5,3 +13,5 @@ curl -sfL https://get.k3s.io | \
   K3S_TOKEN=${TOKEN} \
   INSTALL_K3S_EXEC="--node-ip=192.168.56.111 --flannel-iface=eth1" \
   sh -s -
+
+echo "K3s agent installed."
