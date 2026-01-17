fc9c16fca0925808a288d26cbb07099e49795f7b
ghitakb <ghitakarib97@gmail.com>
2026-01-17 14:53:58 +0100

    Replace p2 with new implementation.

--- p1/scripts/WorkerSetup.sh
+++ p1/scripts/WorkerSetup.sh
@@ -6,6 +6,9 @@ sudo apt update -y
 sudo apt install -y curl ca-certificates
 
 echo "=== Reading master K3s info ==="
+# while [ ! -f /vagrant/node-token ]; do
+#   sleep 2
+# done
 export K3S_URL="https://192.168.56.110:6443"
 export K3S_TOKEN="$(cat /vagrant/node-token)"
 
---
+++ p2/scripts/ServerSetup.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+apt update -y
+apt install -y curl ca-certificates
+
+curl -sfL https://get.k3s.io | \
+INSTALL_K3S_EXEC="server \
+  --node-ip 192.168.56.110 \
+  --write-kubeconfig-mode 0644" sh -
+
+sleep 10
+
+kubectl apply -f /vagrant/confs
+
+
+
+
+# sudo systemctl status k3s --no-pager
+# kubectl get all
+# kubectl get nodes
+# kubectl get deployments
+# kubectl get pods
+# kubectl get services
+# kubectl get ingress
+# kubectl get svc
+
+# curl -H "Host: app1.com" http://192.168.56.110
+# curl -H "Host: app2.com" http://192.168.56.110
+# curl http://192.168.56.110
